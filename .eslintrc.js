b8538819fde560ee481e0698fc9fd81601863fc4
MariamLliso <contacto.mariamlliso@gmail.com>
2022-05-13 17:40:11 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
