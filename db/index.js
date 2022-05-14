f335d2527fa4954b5cec8567974c4f373ebd9aeb
MariamLliso <contacto.mariamlliso@gmail.com>
2022-05-14 18:07:24 +0200

    Created connection to database

--- db/index.js
+++ db/index.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 const debug = require("debug")("robots-api:connection-database");
 const chalk = require("chalk");
 
-const connectionToDatabase = (connectionString) => {
-  const connectionPromise = new Promise();
+const connectionToDatabase = ({ username, password, database }) => {
+  const connectionDBString = `mongodb+srv://${username}:${password}@mariamcluster.xiw3d.mongodb.net/${database}`;
+
+  let connectionPromise = null;
+
   try {
-    connectionPromise((resolve, reject) => {
-      mongoose.connect(connectionString, (error) => {
+    connectionPromise = new Promise((resolve, reject) => {
+      mongoose.connect(connectionDBString, (error) => {
         if (error) {
           debug(chalk.red("Error connecting: ", error.message));
           reject();
@@ -18,8 +21,10 @@ const connectionToDatabase = (connectionString) => {
       });
     });
   } catch (error) {
-    debug(chalk.red("Error at connectionPromise: ", error.message));
+    debug(chalk.red("Error at connection Promise: ", error.message));
   }
+
+  return connectionPromise;
 };
 
 module.exports = connectionToDatabase;
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+require("dotenv").config();
+const debug = require("debug")("robots-api:root");
+const chalk = require("chalk");
+const connectionToDatabase = require("./db");
 
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const database = process.env.MONGO_DATABASE;
+
+(async () => {
+  try {
+    await connectionToDatabase({ username, password, database });
+  } catch {
+    debug(chalk.red("Error initializing server"));
+  }
+})();
